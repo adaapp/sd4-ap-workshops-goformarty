c8f79d67678e0e465a7bc148ad9cc23b71d812d4
Marty Stepien <marty.stepien@gmail.com>
2019-07-02 11:56:51 +0100

    task1

---
+++ encapsulation-workshop/YearGroup.js
@@ -0,0 +1,14 @@
+class YearGroup {
+    constructor(year, headOfYear) {
+        this.year = year;
+        this.headOfYear = headOfYear;
+    }
+    getYear() {
+        return this.year;
+    }
+    getHeadOfYear() {
+        return this.headOfYear;
+    }
+}
+
+module.exports = YearGroup;
